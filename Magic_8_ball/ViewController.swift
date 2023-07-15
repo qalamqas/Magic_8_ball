092af9ac73d8186ac4df6b787144216f468871cc
qalamqas <kalamkas@mail.ru>
2023-07-15 13:09:09 +0600

    Initial Commit

---
+++ Magic_8_ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic_8_ball
+//
+//  Created by Каламкас on 15.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
